0876c7f607f8a44418e4f8e0d71fedd2cff43ca7
Assalam <47153777+Sinidonk@users.noreply.github.com>
2024-12-08 00:40:46 +0700

    Update SAllB.js

--- SAllB.js
+++ SAllB.js
@@ -315,3 +315,26 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //
+
+
+// Script untuk mengatur pergantian iframe (utama)
+        let iframeIndex = 0;
+        const iframe1 = document.getElementById('iframe1');
+        const iframe2 = document.getElementById('iframe2');
+
+        function toggleIframes() {
+            if (iframeIndex === 0) {
+                iframe1.style.display = 'block';
+                iframe2.style.display = 'none';
+                iframeIndex = 1;
+            } else {
+                iframe1.style.display = 'none';
+                iframe2.style.display = 'block';
+                iframeIndex = 0;
+            }
+        }
+
+        // Set interval untuk mengganti iframe setiap 10d (10000 ms)
+        setInterval(toggleIframes, 10000);
+
+
